773a83c77a2cd90c78ca60a89b3279e3688d5903
stefan <star4@vaf.no>
2019-03-07 17:20:52 +0100

    setup multi threading

--- server.py
+++ server.py
@@ -1,17 +1,16 @@
 import socket
-import sys
-#import threading
-#import time
-#from queue import Queue
+import threading
+import time
+from queue import Queue
 
-# Setting up multi Threading (incomplete)
-'''NUMBER_OF_THREADS = 2
+NUMBER_OF_THREADS = 2
 JOB_NUMBER = [1, 2]
 queue = Queue()
 all_connections = []
-all_addresses = []'''
+all_addresses = []
 
-# Create socket (allows two computer to connect)
+
+# Create socket
 def socket_create():
 	try:
 		global host
@@ -22,7 +21,6 @@ def socket_create():
 		s = socket.socket()
 	except socket.error as msg:
 		print("Socket creation error: " + str(msg))
-
 
 # Bind socket to port and wait for connection from client
 def socket_bind():
@@ -37,28 +35,18 @@ def socket_bind():
 		print("Socket binding error: " + str(msg) + "\n" + "Retrying...")
 		socket_bind()
 
-# Establish a connection with client (socket must be listening for them)
-def socket_accept():
-	conn, address = s.accept()
-	print("Connection has been established | " + "IP " + address[0] + " | Port " + str(address[1]))
-	send_commands(conn)
-	conn.close()
-
-def send_commands(conn):
-	while True:
-		cmd = input()
-		if cmd == 'quit':
-			conn.close()
-			s.close()
-			sys.exit()
-		if len(str.encode(cmd)) > 0:
-			conn.send(str.encode(cmd))
-			client_response = str(conn.recv(1024), "utf-8")
-			print(client_response, end="")
-
-def main():
-	socket_create()
-	socket_bind()
-	socket_accept()
-
-main()
+# Accept connections from multiple clients and save to list
+def accept_connections():
+	for c in all_connections:
+		c.clolse()
+	del all_connections [:]
+	del all_addresses [:]
+	while 1:
+		try:
+			conn, address = s.accept()
+			conn.setblocking(1)
+			all_connections.append(conn)
+			all_addresses.append(address)
+			print("\nConnection has beemn established: " + address[0])
+		except:
+			print("Error accepting connections")
